967590d79bfc7ca3410c60a08c4347f6eba2cbcc
Muhtalip Dede <muhtalipdede@gmail.com>
2023-06-18 13:42:34 +0300

    Updates

--- _app/immutable/chunks/3.315eeb30.js
+++ _app/immutable/chunks/3.315eeb30.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page.fca24c55.js";import{default as t}from"../entry/about-page.svelte.66b3080d.js";export{t as component,r as universal};
+import{_ as r}from"./_page.da46b06b.js";import{default as t}from"../entry/about-page.svelte.66b3080d.js";export{t as component,r as universal};
--- _app/immutable/chunks/4.44dfe03f.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page.9accc360.js";import{default as t}from"../entry/blog-page.svelte.45b2978b.js";export{t as component,r as universal};
