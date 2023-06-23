71e3976ce18622d05268430dcd9bae2854ce25bc
Muhtalip Dede <extmuhtalipd@ext.d-teknoloji.com.tr>
2023-06-23 11:20:58 +0300

    Updates

---
+++ _app/immutable/chunks/5.36281db2.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.63e20d10.js";import{default as t}from"../entry/blog-_slug_-page.svelte.16d7d23b.js";export{t as component,r as universal};
--- _app/immutable/chunks/5.98ce5da2.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page.992c9f09.js";import{default as t}from"../entry/blog-_slug_-page.svelte.16d7d23b.js";export{t as component,r as universal};
