f68bead712eb70a383b5b7bd5eb83ff97c61d469
Muhtalip Dede <muhtalipdede@gmail.com>
2024-08-11 16:22:09 +0300

    chore: Update Next.js configuration for exporting and ignoring API folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: process.env.NEXT_OUTPUT_MODE,
+    webpack: (config) => {
+        if (process.env.NEXT_OUTPUT_MODE !== "export" || !config.module) {
+            return config;
+        }
+        config.module.rules?.push({
+            test: /src\/app\/api/,
+            loader: "ignore-loader",
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
