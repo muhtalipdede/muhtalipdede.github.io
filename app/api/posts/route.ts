75cdf06fa0b9493890425324b803619ece39ae4a
Muhtalip Dede <muhtalipdede@gmail.com>
2024-08-11 16:30:53 +0300

    chore: Remove unused dynamic variable and update Next.js configuration

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic';
-
 export function GET(request: Request) {
     return new Response(JSON.stringify([
         { id: 1, title: "Hello World", body: "This is a blog post", author: "Muhtalip Dede", date: "2024-07-01", tags: ["software", "technology"], comments: [] },
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-        // ignore pages/api folder in NEXT_OUTPUT_MODE=export
-        if (process.env.NEXT_OUTPUT_MODE === 'export') {
-            config.module.rules.push({
-                test: /pages[\\/]api[\\/].*/,
-                use: defaultLoaders.babel,
-                exclude: /node_modules/,
-            });
-        }
-        return config;
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
